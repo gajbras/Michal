5c6c72dde3ef06a2fa6f15dcfab3ce40c7f6310a
gajbras <62952568+gajbras@users.noreply.github.com>
2021-10-19 13:11:02 +0200

    commit

--- ukol.ps1
+++ ukol.ps1
@@ -1,13 +1,18 @@
-$json = Get-Content -Path .\Documents\test.json 
+$json = Get-Content -Path .\test.json 
 $object = $json | ConvertFrom-Json
 #$object.zadani
 $object.timestamp.ToString($null)
 $time = [datetime]::ParseExact($object.timestamp.ToString($null),"yyyyMMddHHmmss",$null)
 $time
 $object.data | foreach{
-    #$name = $_.psobject.properties.Name #- vypise jmena objekta
-    $_.psobject.properties.Value | where status -eq "true"
-}
-#$object.data.psobject.Properties - vypise properties objektu
+    #$name = $_.psobject.properties.Value #- vypise jmena objekta
+    $obj_true = $_.psobject.properties.Value | where status -eq "true" 
+    $obj_true = Get-Alias | measure
+    $obj_true.Count
+    $garbage = $_.psobject.properties.Value | where status -eq "true" | Measure-Object garbage -Character
+    $garbage
+    }
+#$object.data.psobject.Properties #- vypise properties objektu
+
 
 #test1: 3 = 49
